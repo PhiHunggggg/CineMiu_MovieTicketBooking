1b84a6d662ad1630279fe83cdbd3cafbfa51044c
Hung <phihung13105@gmail.com>
2026-06-10 08:54:07 +0700

    Đổi đường dẫn chút

--- Repository/SqlServerDbContextDFactory.cs
+++ Repository/SqlServerDbContextDFactory.cs
@@ -11,7 +11,7 @@ namespace Repository
 
             // Used by "dotnet ef" when no startup project host is available.
             var connectionString =
-                "Server=MEICHAN;Database=CINEMIU_MovieTicketBooking;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False";
+                "Server=PHIHUNG\\SQLEXPRESS01;Database=CINEMIU_MovieTicketBooking;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False";
 
             optionsBuilder.UseSqlServer(connectionString);
 
