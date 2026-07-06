ddb944ffc298f055645f223da47d8723436fbee4
Mai Ngọc <ngocmai231105@gmail.com>
2026-07-06 07:29:16 +0700

    Xóa folder ApiGateway lỗi

--- PromotionServiceNode/src/routes/promotion.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const promotionController = require("../controllers/promotion.controller");
-
-router.get('/', promotionController.getAll);
-router.post('/validate', promotionController.validate);
-
-module.exports = router;
